c0703ea7a5d98303f5a3e8e384aa31dfda7ef91b
0velychk0 <Alex.Velichko@gmail.com>
2021-03-05 18:00:52 +0200

    add class MergeSorter

--- sortTest.java
+++ sortTest.java
@@ -9,59 +9,7 @@ class SortTest {
 
     private static Logger logger = Logger.getLogger(SortTest.class.getName());
 
-    // 4. Merge Sort
-    public static void merge(int[] array, int left, int mid, int right) {
-        // вычисляем длину
-        int lengthLeft = mid - left + 1;
-        int lengthRight = right - mid;
-    
-        // создаем временные подмассивы
-        int leftArray[] = new int [lengthLeft];
-        int rightArray[] = new int [lengthRight];
-    
-        // копируем отсортированные массивы во временные
-        for (int i = 0; i < lengthLeft; i++)
-            leftArray[i] = array[left+i];
-        for (int i = 0; i < lengthRight; i++)
-            rightArray[i] = array[mid+i+1];
-    
-        // итераторы содержат текущий индекс временного подмассива
-        int leftIndex = 0;
-        int rightIndex = 0;
-    
-        // копируем из leftArray и rightArray обратно в массив  
-        for (int i = left; i < right + 1; i++) {
-            // если остаются нескопированные элементы в R и L, копируем минимальный
-            if (leftIndex < lengthLeft && rightIndex < lengthRight) {
-                if (leftArray[leftIndex] < rightArray[rightIndex]) {
-                    array[i] = leftArray[leftIndex];
-                    leftIndex++;
-                }
-                else {
-                    array[i] = rightArray[rightIndex];
-                    rightIndex++;
-                }
-            }
-            // если все элементы были скопированы из rightArray, скопировать остальные из leftArray
-            else if (leftIndex < lengthLeft) {
-                array[i] = leftArray[leftIndex];
-                leftIndex++;
-            }
-            // если все элементы были скопированы из leftArray, скопировать остальные из rightArray
-            else if (rightIndex < lengthRight) {
-                array[i] = rightArray[rightIndex];
-                rightIndex++;
-            }
-        }
-    }
-
-    public static void mergeSort(int[] array, int left, int right) {  
-        if (right <= left) return;
-        int mid = (left+right)/2;
-        mergeSort(array, left, mid);
-        mergeSort(array, mid+1, right);
-        merge(array, left, mid, right);
-    }
+ 
 
     // 5. Fast Sort
     public static int partition(int[] array, int begin, int end) {  
@@ -105,7 +53,7 @@ class SortTest {
     public static void main(String[] args) {
         Random random = new Random();
         int[] arrayToSort = random.ints(100000, 10,100000).toArray();
-// ****************
+// **************** Bubble Sort
         int[]  array = arrayToSort.clone();
 
         long before = System.currentTimeMillis();
@@ -121,7 +69,7 @@ class SortTest {
 
         logger.log(Level.INFO, "Bubble Sort Time: {0}", (after - before));
 
-// ****************
+// **************** Insertion Sort
         array = arrayToSort.clone();
 
         before = System.currentTimeMillis();
@@ -136,7 +84,7 @@ class SortTest {
         }
 
         logger.log(Level.INFO, "Insertion Sort Time: {0}", (after - before));
-// ****************
+// **************** Selection Sort
         array = arrayToSort.clone();
 
         before = System.currentTimeMillis();
@@ -151,12 +99,12 @@ class SortTest {
         }
 
         logger.log(Level.INFO, "Selection Sort Time: {0}", (after - before));
-// **************** mergeSort
+// **************** Merge Sort
         array = arrayToSort.clone();
 
         before = System.currentTimeMillis();
 
-        mergeSort(array, 0, array.length-1);
+        MergeSorter.sort(array);
 
         after = System.currentTimeMillis();
 
@@ -166,7 +114,7 @@ class SortTest {
         }
 
         logger.log(Level.INFO, "Merge Sort Time: {0}", (after - before));
-// **************** quickSort
+// **************** Quick Sort
         array = arrayToSort.clone();
 
         before = System.currentTimeMillis();
